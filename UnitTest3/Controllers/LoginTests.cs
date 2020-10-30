22d3e3adfeb379768b64ea1cbc4c2eec1b3de44f
veliihsanyavcin <ihsanyavcin@gmail.com>
2020-10-30 23:40:39 +0300

    Added Unit Test Class

---
+++ UnitTest3/Controllers/LoginTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestApi2.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest.Tests
+{
+    [TestClass()]
+    public class LoginTests
+    {
+        [TestMethod()]
+        public void LoginPostTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ UnitTest3/Controllers/ParkInTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestApi2.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest.Tests
+{
+    [TestClass()]
+    public class ParkInTests
+    {
+        [TestMethod()]
+        public void ParkInPostTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ParkInGetTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
